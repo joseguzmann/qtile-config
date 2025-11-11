2c6f6d1bf1d35b3a9e2eb5991b937eb606a9686d
Jose Guzman <jg175415@gmail.com>
2025-11-11 10:52:08 -0500

    rofi show run

--- config.py
+++ config.py
@@ -81,6 +81,7 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "d", lazy.spawn("rofi -show run"), desc="Show Rofi (run)"),
 ]
 
 # Add key bindings to switch VTs in Wayland.
